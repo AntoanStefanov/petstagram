00abaa1d151fff33c20494cfdc3db49b6d16d63a
AntoanStefanov <antoine.stefanov@gmail.com>
2022-02-03 21:09:34 +0200

    dashboard

--- main/admin.py
+++ main/admin.py
@@ -3,9 +3,12 @@ from main.models import Profile, Pet, PetPhoto
 # Register your models here.
 
 
+class PetInlineAdmin(admin.StackedInline):
+    model = Pet
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    pass
+    inlines = [PetInlineAdmin]
 
 
 @admin.register(Pet)
