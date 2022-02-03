18763dab2831c7b4deefee5e8bb076d8bffad8a6
AntoanStefanov <antoine.stefanov@gmail.com>
2022-02-03 20:55:48 +0200

    project dir

---
+++ petstagram/__init__.py
@@ -0,0 +1,7 @@
+"""
+NEW APP:
+0. Move app inside project dir.
+1. Add new app in settings.py , installed apps.
+2. Create urls.py inside the app with empty urlpatterns.
+3. Include apps's urls.py into project urls.py 
+"""
